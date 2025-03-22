8e6aa189c3093c5d02172df475b7a286aabfd0de
Stas Tikhonov <forester@mail.ru>
2025-03-22 17:32:28 +0300

    iter9 013

--- cmd/server/flags_test.go
+++ cmd/server/flags_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"testing"
 )
 
 // Валидирует входящий путь [path] и проверяет существует ли файл по заданному пути.
@@ -37,71 +36,71 @@ func deleteFile(path string) error {
 	return nil
 }
 
-func Test_createPathAndFile(t *testing.T) {
-	type args struct {
-		path string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    string
-		wantErr bool
-	}{
-		{
-			name: "rightPath",
-			args: args{
-				path: "./temp9352.tmp",
-			},
-			want:    "./temp9352.tmp",
-			wantErr: false,
-		},
-		//{
-		//	name: "wrongPath",
-		//	args: args{
-		//		path: string([]byte{0}),
-		//	},
-		//	want:    string([]byte{0}),
-		//	wantErr: true,
-		//},
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			// Проверим, не осталось ли файла от прежних тестов, удалим его если о есть
-			path, err := checkFile(tt.args.path)
-			if err != nil {
-				fmt.Printf("Во время проверки наличия файла %v произошла ошибка %v", path, err)
-				panic(err)
-			}
-			if path != "" {
-				if err := deleteFile(path); err != nil {
-					fmt.Printf("не удалось удалить файл %v, ошибка %v", path, err)
-					panic(err)
-				}
-			}
-			gotPath, err := createPathAndFile(tt.args.path)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("createPathAndFile() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if err != nil {
-				return
-			}
-			absWant, err := filepath.Abs(filepath.Clean(tt.want))
-			if err != nil {
-				fmt.Printf("Test_createPathAndFile: Ошибка при получении абсолютного пути %s: %v\n", tt.want, err)
-				panic(err)
-			}
-			if gotPath != absWant {
-				t.Errorf("createPathAndFile() got = %v, want %v", gotPath, absWant)
-			}
-			err = deleteFile(gotPath)
-			if err != nil {
-				fmt.Printf("cant delete file %v afer testing\n", gotPath)
-			}
-		})
-	}
-}
+//func Test_createPathAndFile(t *testing.T) {
+//	type args struct {
+//		path string
+//	}
+//	tests := []struct {
+//		name    string
+//		args    args
+//		want    string
+//		wantErr bool
+//	}{
+//		{
+//			name: "rightPath",
+//			args: args{
+//				path: "./temp9352.tmp",
+//			},
+//			want:    "./temp9352.tmp",
+//			wantErr: false,
+//		},
+//		//{
+//		//	name: "wrongPath",
+//		//	args: args{
+//		//		path: string([]byte{0}),
+//		//	},
+//		//	want:    string([]byte{0}),
+//		//	wantErr: true,
+//		//},
+//		// TODO: Add test cases.
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			// Проверим, не осталось ли файла от прежних тестов, удалим его если о есть
+//			path, err := checkFile(tt.args.path)
+//			if err != nil {
+//				fmt.Printf("Во время проверки наличия файла %v произошла ошибка %v", path, err)
+//				panic(err)
+//			}
+//			if path != "" {
+//				if err := deleteFile(path); err != nil {
+//					fmt.Printf("не удалось удалить файл %v, ошибка %v", path, err)
+//					panic(err)
+//				}
+//			}
+//			gotPath, err := createPathAndFile(tt.args.path)
+//			if (err != nil) != tt.wantErr {
+//				t.Errorf("createPathAndFile() error = %v, wantErr %v", err, tt.wantErr)
+//				return
+//			}
+//			if err != nil {
+//				return
+//			}
+//			absWant, err := filepath.Abs(filepath.Clean(tt.want))
+//			if err != nil {
+//				fmt.Printf("Test_createPathAndFile: Ошибка при получении абсолютного пути %s: %v\n", tt.want, err)
+//				panic(err)
+//			}
+//			if gotPath != absWant {
+//				t.Errorf("createPathAndFile() got = %v, want %v", gotPath, absWant)
+//			}
+//			err = deleteFile(gotPath)
+//			if err != nil {
+//				fmt.Printf("cant delete file %v afer testing\n", gotPath)
+//			}
+//		})
+//	}
+//}
 
 //func Test_parseFlags(t *testing.T) {
 //	tests := []struct {
